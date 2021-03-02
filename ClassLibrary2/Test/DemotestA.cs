00262f50ad774bc7aba4f2f967c87b9282b2d257
Ishanu Chatterjee <chatterjeeishanu@yahoo.com>
2021-03-02 20:26:15 +1100

    Add project files.

---
+++ ClassLibrary2/Test/DemotestA.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary2.Utilities;
+using OpenQA.Selenium;
+
+namespace ClassLibrary2.Test
+{
+    class DemotestA
+    {
+        [SetUp]
+        public static void StartTest()
+        {
+            BrowserManagement.LaunchBrowser(TestData.BrowserInput.browser);
+        }
+        [Test]
+        public static void VerifyLaunchURL()
+        {
+            IWebDriver driver = BrowserManagement.driver;
+            Assert.AreEqual(driver.Title, "Google");
+        }
+        [Test]
+        public static void SearchSelenium()
+        {
+            IWebDriver driver = BrowserManagement.driver;
+            driver.FindElement(By.XPath("//input[@title='Search']")).SendKeys("Selenium" + Keys.Enter);
+            Assert.AreEqual(driver.Title, "Selenium - Google Search");
+        }
+        [TearDown]
+        public static void CloseTest()
+        {
+            BrowserManagement.CloseBrowser();
+        }
+    }
+}
---
+++ ClassLibrary2/Utilities/BrowserManagement.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibrary2.Utilities
+{
+    class BrowserManagement
+    {
+        public static IWebDriver driver = Global.CoreDriver.driver;
+        public static void LaunchBrowser(string browser)
+        {
+            switch (browser)
+            {
+                case "Chrome":
+                driver = new ChromeDriver();
+                    break;
+                case "Firefox":
+                    driver = new FirefoxDriver();
+                    break;
+            }
+
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Manage().Window.Maximize();
+            
+        }
+        public static void CloseBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
